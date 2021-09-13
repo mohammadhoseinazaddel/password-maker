be66dfaba06a8fa20febaf4bc73bc88d2b3158cb
mohammad Hosein Azaddel <53640254+mohammadhoseinazad@users.noreply.github.com>
2021-09-13 08:10:39 -0400

    Add files via upload

---
+++ with sys/main.py
@@ -0,0 +1,32 @@
+import string
+import sys
+from random import choices
+
+def create_password(length=5, upper=False, lower=False, digit=False, pun=False):
+    pool=''
+    if upper:
+        pool += string.ascii_uppercase
+    if lower:
+        pool += string.ascii_lowercase
+    if digit:
+        pool += string.digits
+    if pun:
+        pool += string.punctuation
+    if pool == '':
+        pool = string.ascii_letters
+
+    return ''.join(choices(pool, k=length))
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(sys.argv)
+    try:
+        length = int(sys.argv[1])
+    except ValueError:
+        print('Enter integer')
+    else:
+        print(print(create_password(length)))
+
+
